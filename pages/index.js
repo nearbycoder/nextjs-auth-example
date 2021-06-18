c228ee5458a6f106f1ff2543105eea507d82a1a0
Josh Hamilton <nearbycoder@gmail.com>
2021-06-18 17:07:40 -0500

    remove default value

--- pages/index.js
+++ pages/index.js
@@ -125,7 +125,6 @@ function TaskForm({
                     rows={3}
                     className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
                     placeholder="All is great"
-                    defaultValue={''}
                   />
                 </div>
               </div>
