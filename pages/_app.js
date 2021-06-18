3079ab9650ab8e72341b9304aa34f3afc37dcdf9
Josh Hamilton <nearbycoder@gmail.com>
2021-06-18 17:58:36 -0500

    hide panels on mobile

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }) {
           aria-hidden="true"
         />
         <div
-          className="fixed top-0 right-0 w-1/2 h-full bg-gray-50"
+          className="fixed top-0 right-0 w-1/2 h-full hidden sm:fixed"
           aria-hidden="true"
         />
         <div className="relative min-h-screen flex flex-col">
@@ -234,7 +234,7 @@ function MyApp({ Component, pageProps }) {
             {/* Left sidebar & main wrapper */}
             <div className="flex-1 min-w-0 bg-white xl:flex">
               <div className="border-b border-gray-200 xl:border-b-0 xl:flex-shrink-0 xl:w-64 xl:border-r xl:border-gray-200 bg-white">
-                <div className="h-full pl-4 pr-6 py-6 sm:pl-6 lg:pl-8 xl:pl-0">
+                <div className="h-full pl-4 pr-6 py-6 sm:pl-6 lg:pl-8 xl:pl-0 hidden sm:block">
                   {/* Start left column area */}
                   <div
                     className="h-full relative"
@@ -252,8 +252,8 @@ function MyApp({ Component, pageProps }) {
               </div>
             </div>
 
-            <div className="bg-gray-50 pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
-              <div className="h-full pl-6 py-6 lg:w-80">
+            <div className="pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
+              <div className="h-full pl-6 py-6 lg:w-80 hidden sm:block">
                 {/* Start right column area */}
                 <div className="h-full relative" style={{ minHeight: '16rem' }}>
                   <div className="absolute inset-0 border-2 border-gray-200 border-dashed rounded-lg" />
